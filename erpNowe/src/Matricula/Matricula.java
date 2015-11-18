22de62920371fbd4415a9df5a8afc3463e950c11
POO MAÑANAS <POO MAANAS@hp12>
2015-11-18 13:51:04 +0100

    He creado el paquete matricula con su clase

---
+++ erpNowe/src/Matricula/Matricula.java
@@ -0,0 +1,91 @@
+package Matricula;
+
+/**
+ * @author Vanessa
+ * @version 1.0
+ * @since 18/11/2015
+ */
+
+/**
+ *  definir los atributos miembros de la clase
+ *
+ */
+public class Matricula {
+	private int idAlumno;
+	private int idGrupo;
+	private String formaPago;
+	private String promociones;
+	private int pagado;
+	
+/**
+ * sobrecarga de constructotes constructor parametrizado
+ */
+
+	public Matricula(int idAlumno, int idGrupo, String formaPago, String promociones, int pagado) {
+		this.idAlumno = idAlumno;
+		this.idGrupo = idGrupo;
+		this.formaPago = formaPago;
+		this.promociones = promociones;
+		this.pagado = pagado;
+	}
+
+	
+	public int getIdAlumno() {
+		return idAlumno;
+	}
+	
+	public int getIdGrupo() {
+		return idGrupo;
+	}
+	
+	public String getFormaPago() {
+		return formaPago;
+	}
+	
+	public String getPromociones() {
+		return promociones;
+	}
+	
+	public int getPagado() {
+		return pagado;
+	}
+	
+	public void setIdAlumno(int idAlumno) {
+		this.idAlumno = idAlumno;
+	}
+
+	public void setIdGrupo(int idGrupo) {
+		this.idGrupo = idGrupo;
+	}
+
+	public void setFormaPago(String formaPago) {
+		this.formaPago = formaPago;
+	}
+
+
+	public void setPromociones(String promociones) {
+		this.promociones = promociones;
+	}
+
+
+	public void setPagado(int pagado) {
+		this.pagado = pagado;
+	}
+	
+/**
+ *  Metodo crearPresupuesto, no devuelve nada
+ */
+	 
+	public void crearPresupuesto(){
+		
+	}
+	
+/**
+ * Metodo Consultar, no devuelve nada
+ */
+
+	public void Consultar(){
+		
+	}
+	
+}
