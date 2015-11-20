89833654f470a0e317b92306d84bd7c28f0ebca6
Alberto <POO MAANAS@hp09>
2015-11-20 12:51:29 +0100

    clase cursos

--- erpNowe/src/cursos/Cursos.java
+++ erpNowe/src/cursos/Cursos.java
@@ -11,10 +11,19 @@ import java.util.Scanner;
  * @version 1.0
 */
 	public class Cursos {
-
+		 private static String usuario = "root";
+		 private static String pwd = "root";
+		 private static String bd = "nowedb";
+		 static basedatos.ConexionBaseDatos conexion = null;
+		 static Scanner scanner = new Scanner(System.in);
 		
 	    
-	 
+		 public static void main(String[] args) {
+			 // En caso de no tener contraseña pwd = ""
+		    conexion = new basedatos.ConexionBaseDatos(bd, usuario, pwd);
+		    VentanaCursos ventana=new VentanaCursos();
+		    ventana.setVisible(true);
+		}
 	 
 	
 		
@@ -23,7 +32,7 @@ import java.util.Scanner;
 		public static String Consultar(){
 		 	ResultSet datos;
 		    String campo1, campo2, campo3,campo4,campo5,campo6,campo7,campo8, resultado ="";
-		    datos = Principalcursos.conexion.getQuery("SELECT * FROM cursos;");
+		    datos = conexion.getQuery("SELECT * FROM cursos;");
 		    try {
 		      while(datos.next()){//es el metodo que permite ir recorriendo los datos
 		    	  campo1 = datos.getString("idCurso"); //podemos poner el nombre o un numero
@@ -47,7 +56,7 @@ import java.util.Scanner;
 		public static String Consultarip(int idCurso) {
 			ResultSet datos;
 		    String campo1, campo2, campo3,campo4,campo5,campo6,campo7,campo8, resultado ="";
-		    datos = Principalcursos.conexion.getQuery("SELECT * FROM cursos where idCurso = '"+idCurso+"'");
+		    datos = conexion.getQuery("SELECT * FROM cursos where idCurso = '"+idCurso+"'");
 		    try {
 		      while(datos.next()){//es el metodo que permite ir recorriendo los datos
 		    	  campo1 = datos.getString("idCurso"); //podemos poner el nombre o un numero
@@ -74,7 +83,7 @@ import java.util.Scanner;
 		 boolean ok = false;
 		 String resultado = null;
 		 try {
-				 ok = Principalcursos.conexion.setQuery("INSERT INTO cursos(CodigoCurso ,Nombre ,Categoria ,Descripcion ,Precio ,Duracion ,Privado)"
+				 ok = conexion.setQuery("INSERT INTO cursos(CodigoCurso ,Nombre ,Categoria ,Descripcion ,Precio ,Duracion ,Privado)"
 						+ " VALUES ('" + codigoCurso + "','" + nombre  + "','" 
 				 		+ categoria + "','" +  descripcion + "','" +  precio + "','" +  duracion + "','" +  privado  + "' )");
 				 if (ok) 
@@ -94,7 +103,7 @@ import java.util.Scanner;
 		 String resultado = null;
 		 
 		 try{
-			 ok = Principalcursos.conexion.setQuery("UPDATE cursos set CodigoCurso  = '"+codigoCurso+"' ,Nombre = '"+nombre+"' ,Categoria = '"
+			 ok = conexion.setQuery("UPDATE cursos set CodigoCurso  = '"+codigoCurso+"' ,Nombre = '"+nombre+"' ,Categoria = '"
 			 		 +categoria+ "',Descripcion = '"+descripcion+"',Precio = '"+precio+"',Duracion = '"+duracion+"',Privado = '"+privado+"'where idCurso = '"+idcurso+"'");
 			 if (ok) 
 				 resultado="Se modificó registro correctamente";
@@ -113,7 +122,7 @@ import java.util.Scanner;
 		 String resultado = null;
 		 
 		 try{
-			 ok = Principalcursos.conexion.setQuery("DELETE FROM cursos WHERE idCurso = '"+idCursos+"'");
+			 ok = conexion.setQuery("DELETE FROM cursos WHERE idCurso = '"+idCursos+"'");
 			 if (ok) 
 				 resultado="Se borró el registro correctamente";
 			 else 
