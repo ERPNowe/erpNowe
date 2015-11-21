a205b26601a5ae0658afd771cb296b6616254a60
POO MAÑANAS <POO MAANAS@hp11>
2015-11-21 10:53:18 +0100

    actualizacion

--- erpNowe/src/Matricula/Matricula.java
+++ erpNowe/src/Matricula/Matricula.java
@@ -30,7 +30,6 @@ public class Matricula {
 /**
  * sobrecarga de constructotes constructor parametrizado
  */
-
 	public Matricula(int idAlumno, int idGrupo, String formaPago, String promociones, int pagado) {
 		this.idAlumno = idAlumno;
 		this.idGrupo = idGrupo;
@@ -39,7 +38,6 @@ public class Matricula {
 		this.pagado = pagado;
 	}
 
-	
 	public int getIdAlumno() {
 		return idAlumno;
 	}
@@ -83,49 +81,6 @@ public class Matricula {
 	}
 	
 /**
- *  Metodo crearPresupuesto, aun en proceso, ****NO TERMINADO****
- */
-	
-	public static String crearPresupuesto(){
-		
-		float presupuesto= 1000.0F;
-		float descsitulabo = 20.00F;
-		float desctipopago = 5.0F;
-		float presupuestoFinal = presupuesto;
-		String tipoCurso;
-		String situacionLaboral;
-		String tipoPago;
-		String resultado;
-		
-		//esto se borrará, es solo para comprobar los casos
-		tipoCurso = "privado";
-		situacionLaboral = "desempleado";
-		tipoPago = "PagoUnico";
-		
-		if (tipoCurso == "publico"){
-			presupuestoFinal = 0;
-		}
-		else {
-			if (situacionLaboral == "desempleado"){
-				presupuestoFinal = presupuesto - ((presupuesto*descsitulabo)/100);
-				if (tipoPago == "PagoUnico"){
-					presupuestoFinal = presupuesto - ((presupuesto*(descsitulabo + desctipopago))/100);
-				}
-				
-			}else {
-				if (tipoPago == "PagoUnico"){
-				presupuestoFinal = presupuesto - ((presupuesto * desctipopago)/100);}
-			}
-		}
-		resultado = "el presupuesto sin descuento es = " + presupuesto+ "\n" +" y el presupuesto "
-				     + " final que te queda con descuento = " + presupuestoFinal;
-		
-		return resultado;
-						
-	}//fin del metodo crearPresupuesto
-	
-/**
-
  * Metodo ConsultarID
  * Consulta las matriculas del alumno que hemos indicado
  * @return el resultado de la consulta
@@ -202,25 +157,25 @@ public class Matricula {
 
 		public static String Consultar(){
 			ResultSet datos;
-		    String campo1, campo2, campo3, campo4,campo5, resultado ="";
+		    String campo1, campo2, campo3, campo4, campo5, campo6, resultado ="";
 		    datos = principal.conexion.getQuery("SELECT * FROM matricula");
 		    try {
 		      while(datos.next()){//es el metodo que permite ir recorriendo los datos
 		    	  campo1 = datos.getString("idAlumno"); //podemos poner el nombre o un numero
 		    	  campo2 = datos.getString("idGrupo");
 		    	  campo3 = datos.getString("FormaPago");
-		    	  campo4 = datos.getString("Promociones");
-		    	  campo5 = datos.getString("Pagado");
+		    	  campo4 = datos.getString("Desempleado");
+		    	  campo5 = datos.getString("Promociones");
+		    	  campo6 = datos.getString("Pagado");
 		    	  
-		    	  if (campo5=="1")
-		    		  campo5="Pagado";
-		    	  else campo5="No Pagado";
+		    	  if (campo6=="1")
+		    		  campo6="Pagado";
+		    	  else campo6="No Pagado";
 		    	  
 		    	  resultado = resultado +"\n"+"Alumno: "+campo1 + "\n" 
 		    	    	  +"Grupo: " + campo2 + "\n"
 		    	    	  +"forma de pago: " + campo3 + "\n"
-		    	    	  + campo4 + "\n"
-		    	    	  + campo5 + "\n"+ "\n"; 
+		    	    	  + campo4 + "\n" + campo5 + "\n" + campo6 + "\n"; 
 		      }
 		    }
 		    catch (SQLException e) { e.printStackTrace();
