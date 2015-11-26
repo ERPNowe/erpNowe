47145fef8b877430cc41c6161b775bcdf6da028b
POO MAÑANAS <POO MAANAS@hp13>
2015-11-26 13:04:49 +0100

    cambios factura

--- erpNowe/src/utilidades/Factura.java
+++ erpNowe/src/utilidades/Factura.java
@@ -21,6 +21,13 @@ public class Factura {
 	private float baseimponible = 0.0F;
 	private String formaPago;
 	
+	/**
+	 * Metodo constructor
+	 * @param int idfactura, int anofactura, int facturascont, float totfactura, String fechafact, String nomempresa,
+	 * @param String direccempresa, String cifempresa, String personacontacto, String telffaxempresa, float baseimponible,
+	 * @param String formaPago
+	 * @return void
+	 */
 	
 	public void Factura(int idfactura,
 						int anofactura,
@@ -48,14 +55,89 @@ public class Factura {
 		this.baseimponible = baseimponible;
 		this.formaPago = formaPago;
 	}
+
 	
 	
+	public void setIdfactura(int idfactura) {
+		this.idfactura = idfactura;
+	} 	
+	public void setAnoFactura(int anofactura) {
+		this.anofactura = anofactura;
+	} 
+	public void setFacturasCont(int facturascont) {
+		this.facturascont = facturascont;
+	}
+	public void setTotFactura(float totfactura) {
+		this.totfactura = totfactura;
+	}	
+	public void setFechafact(String fechafact) {
+		this.fechafact = fechafact;
+	}
+	public void setNomempresa(String nomempresa) {
+		this.nomempresa = nomempresa;
+	}	
+	public void setDireccempresa(String direccempresa) {
+		this.direccempresa = direccempresa;
+	}
+	public void setCifempresa(String cifempresa) {
+		this.cifempresa = cifempresa;
+	}	
+	public void setPersonacontacto(String personacontacto) {
+		this.personacontacto = personacontacto;
+	}
+	public void setTelffaxempresa(String telffaxempresa) {
+		this.telffaxempresa = telffaxempresa;
+	}	
+	public void setBaseimponible(Float baseimponible) {
+		this.baseimponible = baseimponible;
+	}
+	public void setFormaPago(String formaPago) {
+		this.formaPago = formaPago;
+	}
+	
+	
+	
+	public int getIdfactura(int idfactura) {
+		return this.idfactura;
+	} 	
+	public int getAnoFactura(int anofactura) {
+		return this.anofactura;
+	} 
+	public int getFacturasCont(int facturascont) {
+		return this.facturascont;
+	}
+	public float getTotFactura(float totfactura) {
+		return this.totfactura;
+	}	
+	public String getFechafact(String fechafact) {
+		return this.fechafact;
+	}
+	public String getNomempresa(String nomempresa) {
+		return this.nomempresa;
+	}	
+	public String getDireccempresa(String direccempresa) {
+		return this.direccempresa;
+	}
+	public String getCifempresa(String cifempresa) {
+		return this.cifempresa;
+	}	
+	public String getPersonacontacto(String personacontacto) {
+		return this.personacontacto;
+	}
+	public String getTelffaxempresa(String telffaxempresa) {
+		return this.telffaxempresa;
+	}	
+	public Float getBaseimponible(Float baseimponible) {
+		return this.baseimponible;
+	}
+	public String getFormaPago(String formaPago) {
+		return this.formaPago;
+	}
 	
 	
 	/**
-	 * Metodo para modificar entradas de la base de datos
-	 * @param idCursos
-	 * @param codigoCurso, nombre, categoria, descripcion, precio , duracion , privado
+	 * Metodo para ......
+	 * @param .., ...., 
 	 * @return
 	 */
 	
