7d3569e4ce320861a11d19aa3779f74eca2f0671
POO MAÑANAS <POO MAANAS@hp08>
2015-11-30 12:57:22 +0100

    Subida Clase Calificaciones terminada e implementada con el panel principal.

--- erpNowe/src/alumnos/Calificaciones.java
+++ erpNowe/src/alumnos/Calificaciones.java
@@ -17,111 +17,153 @@ package alumnos;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
 import alumnos.VentanaCalificaciones;
 import ventanaPrincipal.VentanaPrincipal;
 
 public class Calificaciones {
 
-	
-	public int idAlumno;
-	public String idModulo;
-	public float notas;
-	public boolean diplomaRecogido;
-	public String observaciones;
-	
-	
-	
-	public Calificaciones(int idAlumno, String idModulo, float notas, boolean diplomaRecogido, String observaciones) {
-		super();
-		this.idAlumno = idAlumno;
-		this.idModulo = idModulo;
-		this.notas = notas;
-		this.diplomaRecogido = diplomaRecogido;
-		this.observaciones = observaciones;
-	}
-	 public static void main(String[] args) {
+//	private static String usuario = "root";
+//	private static String pwd = "root";
+//	private static String bd = "nowedb" + "";
+	//static VentanaPrincipal.conexionBaseDatos VentanaPrincipal.conexion = null;
+	static boolean diploma = false;
+	static String Obs;
+	static String N;
+
+	public static void main(String[] args) {
+//		VentanaPrincipal.conexion = new VentanaPrincipal.conexionBaseDatos(bd, usuario, pwd);
 		VentanaCalificaciones ventana=new VentanaCalificaciones();
-	    ventana.setVisible(true);
+		ventana.setVisible(true);
 	}
 
-	public int getIdAlumno() {
-		return idAlumno;
-	}
+	/**
+	 * Metodo de consultas en un solo boton.
+	 */
 
-
-
-	public void setIdAlumno(int idAlumno) {
-		this.idAlumno = idAlumno;
-	}
-
-
-
-	public String getIdModulo() {
-		return idModulo;
-	}
-
-
-
-	public void setIdModulo(String idModulo) {
-		this.idModulo = idModulo;
-	}
-
-
-
-	public float getNotas() {
-		return notas;
-	}
-
-
-
-	public void setNotas(float notas) {
-		this.notas = notas;
-	}
-
-
-
-	public boolean isDiplomaRecogido() {
-		return diplomaRecogido;
-	}
-
-
-
-	public void setDiplomaRecogido(boolean diplomaRecogido) {
-		this.diplomaRecogido = diplomaRecogido;
-	}
-
-
-
-	public String getObservaciones() {
-		return observaciones;
-	}
-
-
-
-	public void setObservaciones(String observaciones) {
-		this.observaciones = observaciones;
-	}
-	
 	public static String Consultar(){
-	 	ResultSet datos;
-	    String campo1, campo2, campo3,campo4,campo5,resultado ="";
-	    datos = VentanaPrincipal.conexion.getQuery("SELECT idAlumno,idModulo FROM alumnos,modulos;");
-	    try {
-	      while(datos.next()){//es el metodo que permite ir recorriendo los datos
-	    	  campo1 = datos.getString("idCurso"); //podemos poner el nombre o un numero
-	    	  campo2 = datos.getString("CodigoCurso");
-	    	  campo3= datos.getString("Notas");
-	    	  campo4= datos.getString("Recogido");
-	    	  campo5= datos.getString("Observaciones");
-	    	  resultado = resultado +"\n"+ campo1 + "\t" + campo2 + "\t" + campo3
-	    			  + "\t" + campo4 + "\t" + campo5 + "\t"  ; 
-	      }
-	    }
-	    catch (SQLException e) { e.printStackTrace();
-	   }
-	    return resultado; 
- }
-	
-	
+		ResultSet datos;
+		String dni, codAlumno, codModulo, nota,diplomaR,anotaciones,resultado ="";
+		datos = VentanaPrincipal.conexion.getQuery("SELECT a.DNI,c.* FROM alumnos a,calificaciones c WHERE a.idAlumno = c.idAlumno ORDER BY a.DNI;");
+		try {
+			while(datos.next()){
+				codAlumno = datos.getString("idAlumno");
+
+				dni = datos.getString("DNI");
+				codModulo = datos.getString("idModulo");
+				nota= datos.getString("Notas");
+				diplomaR= datos.getString("DiplomaRecogido");
+				anotaciones= datos.getString("Observaciones");
+				resultado = resultado +"\n"+ codAlumno + "\t"+ dni + "\t" + codModulo + "\t" + nota
+						+ "\t" + diplomaR + "\t" + anotaciones + "\t"  ;	   
+			}
+		}
+		catch (SQLException e) { 
+			e.printStackTrace();
+		}
+		return resultado; 
+	}
+
+	public static String Consultar1(String alumno){
+		ResultSet datos;
+		String dni,codAlumno, codModulo, nota,diplomaR,anotaciones,resultado ="";
+		datos = VentanaPrincipal.conexion.getQuery("SELECT a.DNI,c.* FROM alumnos a,calificaciones c WHERE a.idAlumno = c.idAlumno AND c.idAlumno ='"+alumno+"' ORDER BY a.DNI;");
+		//datos = VentanaPrincipal.conexion.getQuery("SELECT * FROM calificaciones where idAlumno ='"+idAlumno+"';");
+		try {
+			while(datos.next()){//es el metodo que permite ir recorriendo los datos
+				codAlumno = datos.getString("idAlumno"); //podemos poner el nombre o un numero
+				dni = datos.getString("DNI");
+				codModulo = datos.getString("idModulo");
+				nota= datos.getString("Notas");
+				diplomaR= datos.getString("DiplomaRecogido");
+				anotaciones= datos.getString("Observaciones");
+				resultado = resultado +"\n"+ codAlumno + "\t" + dni + "\t" + codModulo + "\t" + nota
+						+ "\t" + diplomaR + "\t" + anotaciones + "\t"  ; 
+			}
+		}
+		catch (SQLException e) { 
+			e.printStackTrace();
+		}
+		return resultado; 
+	}
+
+	public static String Consultar2(String idModulo) {
+		ResultSet datos;
+		String dni,codAlumno, codModulo, nota,diplomaR,anotaciones,resultado ="";
+		datos = VentanaPrincipal.conexion.getQuery("SELECT a.DNI,c.* FROM alumnos a,calificaciones c WHERE a.idAlumno = c.idAlumno AND c.idModulo ='"+idModulo+"' ORDER BY a.DNI;");
+
+		try {
+			while(datos.next()){//es el metodo que permite ir recorriendo los datos
+				codAlumno = datos.getString("idAlumno"); //podemos poner el nombre o un numero
+				dni = datos.getString("DNI");
+				codModulo = datos.getString("idModulo");
+				nota= datos.getString("Notas");
+				diplomaR= datos.getString("DiplomaRecogido");
+				anotaciones= datos.getString("Observaciones");
+				resultado = resultado +"\n"+ codAlumno + "\t" + dni + "\t" + codModulo + "\t" + nota
+						+ "\t" + diplomaR + "\t" + anotaciones + "\t"  ;  
+			}
+		}
+		catch (SQLException e) { 
+			e.printStackTrace();
+		}
+		return resultado; 
+	}	
+
+	public static String Consultar3(String alumno,String idModulo){
+		ResultSet datos;
+		String dni,codAlumno, codModulo, nota,diplomaR,anotaciones,resultado ="";
+		//  datos = VentanaPrincipal.conexion.getQuery("SELECT * FROM calificaciones where idAlumno ='"+alumno+"' AND idModulo ='"+idModulo+"';");
+		datos = VentanaPrincipal.conexion.getQuery("SELECT a.DNI,c.* FROM alumnos a,calificaciones c WHERE a.idAlumno = c.idAlumno AND C.idAlumno ='"+alumno+"'  AND c.idModulo ='"+idModulo+"' ORDER BY a.DNI;");
+
+		try {
+			while(datos.next()){//es el metodo que permite ir recorriendo los datos
+				codAlumno = datos.getString("idAlumno"); //podemos poner el nombre o un numero
+				dni = datos.getString("DNI");
+				codModulo = datos.getString("idModulo");
+				nota= datos.getString("Notas");
+				diplomaR= datos.getString("DiplomaRecogido");
+				anotaciones= datos.getString("Observaciones");
+				resultado = resultado +"\n"+ codAlumno + "\t" + dni + "\t" + codModulo + "\t" + nota
+						+ "\t" + diplomaR + "\t" + anotaciones + "\t"  ;  
+				if(diplomaR.equals("1")) diploma = true;
+				Obs = anotaciones;
+				N = nota;
+			}
+		}
+		catch (SQLException e) { 
+			e.printStackTrace();
+		}
+		return resultado; 
+	}
+
+	//---------------------------------------------------------------------	
+	public static String ModificarCalificaciones(
+			String idAlumno,
+			String idModulo,
+			String nota,
+			String observaciones,
+			boolean recibido
+			) {
+		int dRecibido = (recibido)? 1:0;
+		int nota2=Integer.parseInt(nota);
+		String resultado="";
+		boolean ok = false;
+
+
+
+		try {
+			ok= VentanaPrincipal.conexion.setQuery("UPDATE  calificaciones SET Notas = '"+nota2+"' Observaciones = '"+observaciones+"', diplomaRecogido = '" +dRecibido +"' WHERE idAlumno = '"+idAlumno+"'AND idModulo = '"+idModulo+"' ");
+
+			if (ok)
+				resultado = "Se Modifico correctamente";
+
+			else
+				resultado = "Error";
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return resultado;
+	}
+
 }
