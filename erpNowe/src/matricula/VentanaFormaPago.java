deb0dc0769d9f7f57d676d2f800cd37825caa7eb
Vanessa y Victor <POO MAANAS@hp11>
2015-11-30 13:12:18 +0100

    Cambios

--- erpNowe/src/matricula/VentanaFormaPago.java
+++ erpNowe/src/matricula/VentanaFormaPago.java
@@ -26,20 +26,19 @@ import ventanaPrincipal.VentanaPrincipal;
 @SuppressWarnings("serial")
 public class VentanaFormaPago extends JFrame {
 	
-	//creo el constructor del panel donde pongo todas sus propiedades//
 	
-		 public static JButton  modificar = null;
+	
+		public static JButton  modificar = null;
 		public static JButton insertar = null;
 		public static JButton borrar = null;
 
 		public VentanaFormaPago() {
-		    	//para ponerle el icono a la app
 		    	Image icon = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/img/logo_nowe.gif"));
 			    setIconImage(icon);
-			    setSize(780,455); // Ancho*Alto
+			    setSize(780,455); 
 			    setTitle("Gestión Formas de Pago Nowe");
 			    setLocationRelativeTo(null);
-		        Panel p = new Panel(); //crear la clase panel como en marcoprincipal
+		        Panel p = new Panel(); 
 				add(p); 
 			    }
 
@@ -57,7 +56,6 @@ public class VentanaFormaPago extends JFrame {
 		    	 setBackground(new Color(192,192,192));
 		    	 scrollArea = new JScrollPane();
 		    	
-		    	 //creo los botones
 		    	 listar=new JButton("Listado");
 		    	 listar.setBounds(10,10,120,50); 
 		         add(listar);
@@ -96,8 +94,6 @@ public class VentanaFormaPago extends JFrame {
 		         add(cerrar);
 		         cerrar.addActionListener(this);
 		         
-		         
-			 //pongo el textarea
 		         textareaconsulta = new JTextArea();
 		         textareaconsulta.setBackground(new Color(224,224,224));
 		         
@@ -105,10 +101,6 @@ public class VentanaFormaPago extends JFrame {
 		         scrollArea.setBounds(140,10,430,350); //posiciona dentro de la ventana
 		         add(scrollArea);
 			 
-			 
-			 //pongo los texfileds y jlabel
- 
-		         
 		         label1=new JLabel("idFormaPago");
 		         label1.setBounds(580,30,100,20);
 		         add(label1);
@@ -126,11 +118,9 @@ public class VentanaFormaPago extends JFrame {
 		         textfield2 = new JTextField();
 		         textfield2.setBounds(580,70,170,20);
 		         add(textfield2);
-		         textfield2.setBackground( new Color(224,224,224) );
-		         
-		         
+		         textfield2.setBackground( new Color(224,224,224) ); 
 			 }
-		          //creo las acciones
+		          
 			 public void actionPerformed(ActionEvent e) {
 		         	Object botonPulsado = e.getSource();
 		         	
