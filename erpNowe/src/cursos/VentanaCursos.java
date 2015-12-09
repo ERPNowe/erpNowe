1bd423b1cef2e874c8b2b9163b587f9044179fd0
POO MAÑANAS <POO MAANAS@hp09>
2015-12-09 14:02:31 +0100

    cambio en combobox cursos

--- erpNowe/src/cursos/VentanaCursos.java
+++ erpNowe/src/cursos/VentanaCursos.java
@@ -75,7 +75,7 @@ public class VentanaCursos extends JFrame {
 			
 		 class Panel extends JPanel implements ActionListener{
 
-			 JButton listar, insertar, limpiar, cerrar, consultar;
+			 JButton listar, insertar, limpiar, cerrar, consultar, editar;
 			 JTextArea textareaconsulta,textarea5; 
 			 JTextField textfield1, textfield2,textfield3,textfield4,textfield6,textfield7,textfield8;
 			 JLabel label1,label2,label3,label4,label5,label6,label7,label8;
@@ -95,7 +95,13 @@ public class VentanaCursos extends JFrame {
 		    	 /**
 		    	  * Creacion de los botones
 		    	  */
-		         consultar=new JButton("Consultar");
+		         editar = new JButton("editar");
+		         editar.setBounds(610,230,100,50);
+		         add(editar);
+		    	 
+		    	 
+		    	 
+		    	 consultar=new JButton("Consultar");
 		    	 consultar.setBounds(125,10,120,50); //Padding_Left, Padding_Top, Ancho, Alto
 		         add(consultar);
 		         consultar.addActionListener(this);
@@ -187,14 +193,14 @@ public class VentanaCursos extends JFrame {
 		         add(textfield1);
 		         textfield1.setBackground( new Color(224,224,224) );
 		         
-		         label2=new JLabel("Código Curso");
+		         label2=new JLabel("CódigoCurso");
 		         label2.setBounds(265,10,100,20);
 		         add(label2);
 		         
-		         //textfield2=new JTextField();
-		         //textfield2.setBounds(265,30,100,20);
-		         //add(textfield2);
-		         //textfield2.setBackground( new Color(224,224,224) );
+		         textfield2=new JTextField();
+		         textfield2.setBounds(265,30,100,20);
+		         add(textfield2);
+		         textfield2.setBackground( new Color(224,224,224) );
 		         
 		         label3=new JLabel("Nombre");
 		         label3.setBounds(380,10,100,20);
@@ -209,10 +215,10 @@ public class VentanaCursos extends JFrame {
 		         label4.setBounds(495,10,100,20);
 		         add(label4);
 		         
-		         //textfield4=new JTextField();
-		         //textfield4.setBounds(495,30,100,20);
-		         //add(textfield4);
-		         //textfield4.setBackground( new Color(224,224,224) );
+		         textfield4=new JTextField();
+		         textfield4.setBounds(495,30,100,20);
+		         add(textfield4);
+		         textfield4.setBackground( new Color(224,224,224) );
 		         
 		         label5=new JLabel("Descripcion");
 		         label5.setBounds(610,10,100,20);
@@ -258,6 +264,7 @@ public class VentanaCursos extends JFrame {
 		     		e.printStackTrace();
 		     	}
 		     	comboidCurso.addActionListener(this);
+		        comboidCurso.setEnabled(false);
 		         add(comboidCurso);
 		         
 			 
@@ -270,6 +277,7 @@ public class VentanaCursos extends JFrame {
 		     		e.printStackTrace();
 		     	}
 		     	combocategoria.addActionListener(this);
+		     	combocategoria.setEnabled(false);
 		         add(combocategoria);
 			 
 			 
@@ -325,17 +333,16 @@ public class VentanaCursos extends JFrame {
 		             
 		             
 		             if (botonPulsado==consultar){
-		            	clearCursos();
 		            	
-		            	ResultSet filas;
-		     			filas = Cursos.Consultarid(textfield1.getText(), this);
+		            	 ResultSet filas;
+		     			filas = Cursos.Consultarip(textfield1.getText());
 		     	        
 		     			try {
 		     				if (filas.next()){
 		     					
-		     					comboidCurso.setToolTipText(filas.getString("CodigoCurso"));
+		     					textfield2.setText(filas.getString("CodigoCurso"));
 		     					textfield3.setText(filas.getString("Nombre"));
-		     					combocategoria.setToolTipText(filas.getString("Categoria"));
+		     					textfield4.setText(filas.getString("Categoria"));
 		     					textarea5.setText(filas.getString("Descripcion"));
 		     					textfield6.setText(filas.getString("Precio"));
 		     					textfield7.setText(filas.getString("Duracion"));
@@ -367,7 +374,10 @@ public class VentanaCursos extends JFrame {
 		            	 
 		            	 textareaconsulta.setText(Cursos.Insertar(CodigoCurso1,Nombre1,Categoria1,Descripcion1,flprecio,intduracion,intprivado));}
 		            	 
-
+		             
+		            
+		             
+		    		 
 		             //------------------------------------------------------------------
 		             if (botonPulsado==modificar) {
 		            	 String idcurso1 = textfield1.getText();
@@ -385,9 +395,12 @@ public class VentanaCursos extends JFrame {
 		            	 
 		            	
 		            	 textareaconsulta.setText(Cursos.Modificar(intidcurso,CodigoCurso2,Nombre2,Categoria2,Descripcion2,flprecio1,intduracion1,intprivado1));
-		            	 
+		      
+		             
 		             }
-
+		 
+		             
+		             
 		             
 		             //----------------------------------------------------------------------------------------------------------
 		             if (botonPulsado==borrar) {
@@ -399,8 +412,10 @@ public class VentanaCursos extends JFrame {
 		        			 
 		        	//--------------------------------------------------------------------
 		             if (botonPulsado==limpiar) {
-		            	 textfield1.setText(""); 
-		            	 textfield3.setText("");		            
+		            	 textfield1.setText("");
+		            	 textfield2.setText("");
+		            	 textfield3.setText("");
+		            	 textfield4.setText("");
 		            	 textarea5.setText("");
 		            	 textfield6.setText("");
 		            	 textfield7.setText("");
@@ -408,9 +423,15 @@ public class VentanaCursos extends JFrame {
 		            	
 		            	 textareaconsulta.setText("");}
 		             
-/*		             if (botonPulsado==cerrar) {
-		            	 VentanaPrincipal.conexion.cerrarConexion();
-		            	 textareaconsulta.setText("conexion cerrada");}*/
+		             if (botonPulsado==editar) {
+					modificar.setEnabled(true);
+					combocategoria.setEnabled(true);
+					comboidCurso.setEnabled(true);
+		           textfield2.setEnabled(false);
+		           label2.setEnabled(false);
+		           textfield4.setEnabled(false);
+		           label4.setEnabled(false);
+					}
 		            	 
 		           }
 				/**
